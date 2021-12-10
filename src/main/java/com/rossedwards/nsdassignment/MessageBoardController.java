1abd440f6cbfd1934214d31a856ddb3a4e5052c9
Ross Edwards <redwards64@hotmail.com>
2021-12-10 23:47:06 +0000

    getting client to work with gui

--- src/main/java/com/rossedwards/nsdassignment/MessageBoardController.java
+++ src/main/java/com/rossedwards/nsdassignment/MessageBoardController.java
@@ -3,11 +3,20 @@ package com.rossedwards.nsdassignment;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+
 import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class MessageBoardController {
 
+    public Font x1;
+    public Color x2;
+    public Font x11;
+    public Color x21;
+    public Font x3;
+    public Color x4;
     Client client;
     PrintWriter out;
     Scanner reader;
